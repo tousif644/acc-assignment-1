4cb0e8462aa1f98191b75c22f095126d5247999c
Tousif Tasrik <tousif2567@gmail.com>
2022-09-03 21:47:21 +0600

    updated route

--- Routes/v1/user.route.js
+++ Routes/v1/user.route.js
@@ -5,11 +5,58 @@ const router = express.Router();
 const userController = require("../../Controller/user.controller");
 
 router.route('/random').get(userController.getRandomUser);
+
+/**
+* @api {get} /tools All tools
+* @apiDescription Get all the tools
+* @apiPermission admin
+*
+* @apiHeader {String} Authorization   User's access token
+*
+* @apiParam  {Number{1-}}         [page=1]     List page
+* @apiParam  {Number{1-100}}      [limit=10]  Users per page
+*
+* @apiSuccess {Object[]} all the tools.
+*
+* @apiError (Unauthorized 401)  Unauthorized  Only authenticated users can access the data
+* @apiError (Forbidden 403)     Forbidden     Only admins can access the data
+*/
+
+/**
+* @api {post} /tools save a tool
+* @apiDescription Save all the tools
+* @apiPermission admin
+*
+* @apiHeader {String} Authorization   User's access token
+*
+* @apiParam  {Number{1-}}         [page=1]     List page
+* @apiParam  {Number{1-100}}      [limit=10]  Users per page
+*
+* @apiSuccess {Object[]} all the tools.
+*
+* @apiError (Unauthorized 401)  Unauthorized  Only authenticated users can access the data
+* @apiError (Forbidden 403)     Forbidden     Only admins can access the data
+*/
 router.route('/all').get(userController.getAllUsers)
-router.route('/save').post(userController.saveARandomUser)
-router.route('/:id')
+/**
+ * @api {post} /tools save a tool
+ * @apiDescription Save all the tools
+ * @apiPermission admin
+ *
+ * @apiHeader {String} Authorization   User's access token
+ *
+ * @apiParam  {Number{1-}}         [page=1]     List page
+ * @apiParam  {Number{1-100}}      [limit=10]  Users per page
+ *
+ * @apiSuccess {Object[]} all the tools.
+ *
+ * @apiError (Unauthorized 401)  Unauthorized  Only authenticated users can access the data
+ * @apiError (Forbidden 403)     Forbidden     Only admins can access the data
+ */
+router.route('/save').post(userController.saveARandomUser) // gives you the random one data
+router.route('/:id') // can get value by id 
     .get(userController.getUserById)
-    .delete(userController.deleteOneUser)
-router.route('/update/:id').patch(userController.updateRandomUser)
-router.route('/bulk-update/:id/:secondId/:thirdId').patch(userController.bulkUpdate)
+    .delete(userController.deleteOneUser) // delete api by id
+router.route('/update/:id').patch(userController.updateRandomUser) // update user by id
+router.route('/bulk-update/:id/:secondId/:thirdId').patch(userController.bulkUpdate) // update multiple user with id 
 module.exports = router
