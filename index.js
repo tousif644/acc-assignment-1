20f39c7a2c09dfca7ac9b2c0f48447250eebb506
Tousif Tasrik <tousif2567@gmail.com>
2022-09-03 21:43:20 +0600

    Update index.js
    DONE

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 // connect to db
 dbConnect();
 app.use("/api/v1/users", userRoute);
